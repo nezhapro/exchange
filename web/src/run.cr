087e8582249ce6321c3d0546bcc2dbc7043a5b2f
nezhapro <nezhapro@163.com>
2018-02-12 16:07:22 +0800

    up

--- web/src/run.cr
+++ web/src/run.cr
@@ -2,12 +2,14 @@ require "kemal"
 require "sqlite3"
 Kemal.config.port=80
 get "/" do
-  render "src/views/home.ecr"
+  title="哪吒"
+  render "src/views/home.ecr","src/views/layouts/base.ecr"
 end
 
 get "/profile/:name" do |env|
   name=env.params.url["name"]
-  render "src/views/profile.ecr"
+  title="用户档案"
+  render "src/views/profile.ecr","src/views/layouts/base.ecr"
 end
 
 get "/users" do
@@ -19,11 +21,13 @@ get "/users" do
       users=rs
     end
   end
-  render "src/views/users.ecr"
+  title="用户列表"
+  render "src/views/users.ecr","src/views/layouts/base.ecr"
 end
 
 error 404 do
-  render "src/views/404.ecr"
+  title="404"
+  render "src/views/404.ecr","src/views/layouts/base.ecr"
 end
 
 post "/upload" do |env|
@@ -42,7 +46,39 @@ post "/upload" do |env|
 end
 
 get "/upload" do
-  render "src/views/upload.ecr"
+  title="上传图片"
+  render "src/views/upload.ecr","src/views/layouts/base.ecr"
+end
+
+get "/login" do
+  title="登录"
+  render "src/views/login.ecr","src/views/layouts/base.ecr"
+end
+
+get "/register" do
+  title="注册"
+  render "src/views/register.ecr","src/views/layouts/base.ecr"
+end
+
+get "/about" do
+  title="关于"
+  render "src/views/about.ecr","src/views/layouts/base.ecr"
+end
+
+get "/list" do
+  title="列表"
+  render "src/views/list.ecr","src/views/layouts/base.ecr"
+end
+
+get "/news" do
+  title="新鲜事"
+  render "src/views/news.ecr","src/views/layouts/base.ecr"
+end
+
+get "/detail/:id" do |env|
+  id=env.params.url["id"]
+  title="详情"
+  render "src/views/detail.ecr","src/views/layouts/base.ecr"
 end
 
 Kemal.run
